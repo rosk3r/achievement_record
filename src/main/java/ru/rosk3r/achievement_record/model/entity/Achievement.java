fd030113226578be7b5c88e6412ea62e5c4923c3
rosk3r <logthelog@gmail.com>
2023-11-30 11:40:13 +0600

    refactor: add @Table annotation

--- src/main/java/ru/rosk3r/achievement_record/model/entity/Achievement.java
+++ src/main/java/ru/rosk3r/achievement_record/model/entity/Achievement.java
@@ -1,9 +1,6 @@
 package ru.rosk3r.achievement_record.model.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,6 +11,7 @@ import lombok.NoArgsConstructor;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@Table(name = "t_achievements")
 public class Achievement {
 
     private Long eventId;
